9dc7f982aa267758730ebf2bd47ac8c75175a28e
yonggoonoh <nohyg90@gmail.com>
2017-11-29 01:04:51 +0900

    feat: create menu bar using Link component

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Menu from 'components/Menu';
 import { Home, About } from 'pages';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Menu/>
                 <Route exact path="/" component={Home}/>
                 <Switch>
                     <Route path="/about/:name" component={About}/>
