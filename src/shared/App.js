6ed288b54d6eca9805c2eab9eda4428a8b96566f
yonggoonoh <nohyg90@gmail.com>
2017-11-29 01:19:46 +0900

    feat: show posts based on the ids in url params

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Menu from 'components/Menu';
-import { Home, About } from 'pages';
+import { Home, About, Posts } from 'pages';
 
 class App extends Component {
     render() {
@@ -13,6 +13,7 @@ class App extends Component {
                     <Route path="/about/:name" component={About}/>
                     <Route path="/about" component={About}/>
                 </Switch>
+                <Route path="/posts" component={Posts}/>
             </div>
         )
     }
