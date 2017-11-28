5fa3daa321302c7707de04c10bb4b5fcd779ac0d
yonggoonoh <nohyg90@gmail.com>
2017-11-29 00:45:54 +0900

    feat: routing home and about component

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home, About } from 'pages';
 
 class App extends Component {
-    render () {
+    render() {
         return (
             <div>
-                Hello
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
             </div>
         )
     }
